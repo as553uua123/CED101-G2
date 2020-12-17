1e0556879f61b80bd1319ded64e35c4640c71335
s4ndragon <js4ydy@hotmail.com>
2020-12-17 09:31:35 +0800

    update order list & personal profile form

--- js/sandra/membership.js
+++ js/sandra/membership.js
@@ -33,6 +33,38 @@ $(document).ready(function () {
         $("#mine_fav").hide();
         $("#mine_order").hide();
         $("#mine_article").hide();
-        $("#mine_profile").show();
+        $("#mine_profile").css({
+            display: "flex",
+        });
     });
+
+    $("input").on("keyup blur", function () {
+        $("label").on("keyup").addClass("active");
+        // $("label").on("blur").removeClass("active");
+    });
+    // $(".form")
+    //     .find("input, textarea")
+    //     .on("keyup blur focus", function (e) {
+    //         var $this = $(this),
+    //             label = $this.prev("label");
+    //     if (e.type === "keyup") {
+    //         if ($this.val() === "") {
+    //             label.removeClass("active highlight");
+    //         } else {
+    //             label.addClass("active highlight");
+    //         }
+    //     } else if (e.type === "blur") {
+    //         if ($this.val() === "") {
+    //             label.removeClass("active highlight");
+    //         } else {
+    //             label.removeClass("highlight");
+    //         }
+    //     } else if (e.type === "focus") {
+    //         if ($this.val() === "") {
+    //             label.removeClass("highlight");
+    //         } else if ($this.val() !== "") {
+    //             label.addClass("highlight");
+    //         }
+    //     }
+    // });
 });
