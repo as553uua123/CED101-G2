49d2c7d149bf003723b6afd11c0840b16418fc7a
shong7777 <shong_forever@hotmail.com>
2020-12-23 12:11:14 +0800

    使用公制btn

--- js/product.js
+++ js/product.js
@@ -17,9 +17,11 @@ window.addEventListener('load', () => {
                             <h4>【原藝坊】悠然愜意 青瓷鯉魚茶壺套組精湛工藝，功夫淳厚，手感極佳，品茶必備</h4>
                             <div class="price">NT<span>550</span></div>
                         </a>
-                        <input type="button" value="加入收藏" class="add_favorite">
+                        <div class='btns'>
+                        <img class='addFavorite' src="./images/common/heart.png" alt="">
                         <input type="button" value="加入購物車" class="add_cart">
                         <input type="hidden" name="" value='s1123|【原藝坊】悠然愜意 青瓷鯉魚茶壺套組精湛工藝，功夫淳厚，手感極佳，品茶必備|550|1' class='productInfo'>
+                        </div>
                     </div>
                             `;
     }
